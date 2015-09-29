5e160f020cb5beedab83dd90434fb6493d01ec07
cory321 <cory321@gmail.com>
2015-09-29 16:40:02 -0700

    updated styles, added 4 new booking steps, added static pages

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ var express = require("express");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+
+//**** PUBLIC VIEWS ****//
+
 app.get("/", function(req,res){
+	res.render("index");
+});
+
+app.get("/step1", function(req,res){
 	res.render("step1");
 });
 
@@ -16,6 +23,36 @@ app.get("/step2", function(req,res){
 
 app.get("/step3", function(req,res){
 	res.render("step3");
+});
+
+app.get("/step4", function(req,res){
+	res.render("step4");
+});
+
+app.get("/san-luis-obispo-wedding-videography", function(req,res){
+	res.render("films");
+});
+
+app.get("/about-cory-williams", function(req,res){
+	res.render("about");
+});
+
+app.get("/contact-us", function(req,res){
+	res.render("contact");
+});
+
+
+//**** ADMIN DASHBOARD ****//
+
+app.get("/admin", function(req,res){
+	res.render("admin/index");
+});
+
+
+
+//**** 404 Catch All ****//
+app.get("*", function(req,res){
+	res.redirect("/");
 });
 
 app.listen(3000, function(req,res){
