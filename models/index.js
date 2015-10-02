8ef36895890e567d3c4dd6983431a966da623a8c
cory321 <cory321@gmail.com>
2015-10-02 09:13:18 -0700

    procfile and db creds

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/surrealdb");
+mongoose.connect("mongodb://cory321:cory103662@ds051953.mongolab.com:51953/heroku_rqzmw3k7" || "mongodb://localhost/surrealdb");
 mongoose.set("debug", true);
 
 module.exports.User = require("./user");
